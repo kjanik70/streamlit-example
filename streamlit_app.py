a0dadabe7093003505be9371fabca1df5ddf83fc
kjanik70 <31418157+kjanik70@users.noreply.github.com>
2023-07-02 14:42:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,8 @@ import streamlit as st
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
+Are these lines just randomly shown on the app?
+
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
